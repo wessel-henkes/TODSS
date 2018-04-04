6d49e1d63fea6ec9a2a89cafe23cf1ab924ea2ae
wessel <wessel.henkes@casema.nl>
2018-04-04 14:19:41 +0200

    update dialog + webapp naar eigen repo

--- Todss.pml
+++ Todss.pml
@@ -2,13 +2,22 @@
 <Package name="Todss" format_version="4">
     <Manifest src="manifest.xml" />
     <BehaviorDescriptions>
-        <BehaviorDescription name="behavior" src="behavior_1" xar="behavior.xar" />
+        <BehaviorDescription name="behavior" src="dialog_test" xar="behavior.xar" />
+        <BehaviorDescription name="behavior" src="Dabbing_pepper" xar="behavior.xar" />
     </BehaviorDescriptions>
-    <Dialogs />
+    <Dialogs>
+        <Dialog name="basic_gesprek" src="basic_gesprek/basic_gesprek.dlg" />
+        <Dialog name="ExampleDialog" src="dialog_test/ExampleDialog/ExampleDialog.dlg" />
+    </Dialogs>
     <Resources />
-    <Topics />
-    <IgnoredPaths />
-    <Translations auto-fill="en_US">
-        <Translation name="translation_en_US" src="translations/translation_en_US.ts" language="en_US" />
+    <Topics>
+        <Topic name="basic_gesprek_dun" src="basic_gesprek/basic_gesprek_dun.top" topicName="basic_gesprek" language="nl_NL" />
+        <Topic name="ExampleDialog_enu" src="dialog_test/ExampleDialog/ExampleDialog_enu.top" topicName="ExampleDialog" language="en_US" />
+    </Topics>
+    <IgnoredPaths>
+        <Path src=".gitattributes" />
+    </IgnoredPaths>
+    <Translations auto-fill="nl_NL">
+        <Translation name="translation_nl_NL" src="translations/translation_nl_NL.ts" language="nl_NL" />
     </Translations>
 </Package>
---
+++ translations/translation_nl_NL.ts
@@ -0,0 +1,67 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="nl_NL">
+    <context>
+        <name>Dabbing_pepper/behavior.xar:/Animated Say</name>
+        <message>
+            <source>Ik ben peppper</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Ik ben peppper</translation>
+        </message>
+        <message>
+            <source>Ik ben pep per</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Ik ben pep per</translation>
+        </message>
+        <message>
+            <source>ik kan debben</source>
+            <comment>Text</comment>
+            <translation type="obsolete">ik kan debben</translation>
+        </message>
+        <message>
+            <location filename="Dabbing_pepper/behavior.xar" line="0"/>
+            <source>kijk ik kan debben</source>
+            <comment>Text</comment>
+            <translation type="unfinished">kijk ik kan debben</translation>
+        </message>
+    </context>
+    <context>
+        <name>My_First_App/behavior.xar:/Animated Say</name>
+        <message>
+            <source>Hallo</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hallo</translation>
+        </message>
+        <message>
+            <source>kijk ik kan debben</source>
+            <comment>Text</comment>
+            <translation type="obsolete">kijk ik kan debben</translation>
+        </message>
+    </context>
+    <context>
+        <name>je_staat_op_mijn_teen/behavior.xar:/Animated Say</name>
+        <message>
+            <source>Hallo</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hallo</translation>
+        </message>
+        <message>
+            <source>Au je staat op mijn teen</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Au je staat op mijn teen</translation>
+        </message>
+    </context>
+    <context>
+        <name>je_staat_op_mijn_teen/behavior.xar:/Say</name>
+        <message>
+            <source>Hallo</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hallo</translation>
+        </message>
+        <message>
+            <source>Au je staat op mijn teen</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Au je staat op mijn teen</translation>
+        </message>
+    </context>
+</TS>
